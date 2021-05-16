0e1d86520c84a78343c3137bb2350e24b58d730c
Philippe Huet <63857313+philhu22@users.noreply.github.com>
2021-05-16 11:36:12 +0200

    add setup.py, README

---
+++ README.rst
@@ -0,0 +1,5 @@
+===============================
+Google Cloud Helper for Python
+===============================
+A wrapper around the Google Cloud SDK for Python developed with the goal to provide an additional abstraction layer.
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+INSTALL_REQUIRES = [
+      'google-api-core>=1.26.3',
+      'google-cloud-storage>=1.37.1',
+      'google-cloud-automl>=2.3.0',
+      'google-cloud-bigquery>=2.14.0'
+]
+
+setup(
+    name='txpy-gchelper',
+    version='0.0.11',
+    description='A wrapper around the Google Cloud SDK for Python',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Philippe Huet',
+    author_email='philhu@tyxio.com',
+    url='https://github.com/tyxio/txpy-gchelper',
+    license='MIT License',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independant"
+        ]
+)
